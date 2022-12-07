283d8d8284e1ec8bad1c526288975944fbbd3c13
Rafael Amaral de Paula <rafael.paula@sysmap.com.br>
2022-12-07 14:18:34 -0300

    add test package util

---
+++ src/test/java/util/DateUtilsTest.java
@@ -0,0 +1,28 @@
+package util;
+
+import br.ce.wcaquino.taskbackend.utils.DateUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class DateUtilsTest {
+
+    @Test
+    public void deveRetornarTrueParaDataFutura() {
+        LocalDate date = LocalDate.now().plusYears(10);
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataPassada() {
+        LocalDate date = LocalDate.of(2021, 10, 01);
+        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+    }
+
+    @Test
+    public void deveRetornarTrueParaDataAtual() {
+        LocalDate date = LocalDate.now();
+        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+    }
+}
